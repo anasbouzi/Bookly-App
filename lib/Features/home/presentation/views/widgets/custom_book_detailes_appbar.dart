ed9374aa663a3bd4a470ca790d4ea7731e6705f0
Anas <ahmdbwzy14@gmail.com>
2025-02-22 15:09:55 +0300

    create search view

--- lib/Features/home/presentation/views/widgets/custom_book_detailes_appbar.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_detailes_appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBookDetailesAppBar extends StatelessWidget {
   const CustomBookDetailesAppBar({super.key});
@@ -9,7 +10,9 @@ class CustomBookDetailesAppBar extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            GoRouter.of(context).pop();
+          },
           icon: const Icon(Icons.close),
         ),
         IconButton(
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,11 +1,13 @@
 import 'package:bookly/Features/Splash/presentation/views/splash_view.dart';
 import 'package:bookly/Features/home/presentation/views/book_detailes_view.dart';
 import 'package:bookly/Features/home/presentation/views/home_view.dart';
+import 'package:bookly/Features/search/presentation/views/search_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailesView = '/bookDetailes';
+  static const kSearchView='/searchView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -19,6 +21,10 @@ abstract class AppRouter {
       GoRoute(
         path: kBookDetailesView,
         builder: (context, state) => const BookDetailesView(),
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (context, state) => const SearchView(),
       ),
     ],
   );
